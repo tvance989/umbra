79fb3106b54e2b6c5417f10151c5f0c8aa2f4d96
Taylor Vance <tvance989@gmail.com>
2017-03-04 09:46:03 -0600

    Player controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public float speed;
+
+	Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		rb.velocity = movement * speed;
+	}
+}
