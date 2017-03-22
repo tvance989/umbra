9ff8335bf95eb539c13acb78b4e5827201aa0b99
Taylor Vance <tvance989@gmail.com>
2017-03-22 08:07:32 -0500

    Input validation

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -5,9 +5,29 @@ using UnityEngine.UI;
 
 public class GameOverManager : MonoBehaviour {
 	public Text scoreText, highScoreText;
+	public InputField initialsInput;
 
 	void Start () {
 		scoreText.text = "Score: " + ScoreManager.instance.GetScore ();
 		highScoreText.text = "High Score: " + ScoreManager.instance.GetHighScore ();
+
+		initialsInput.onValueChanged.AddListener (delegate {
+			ForceUpper();
+		});
+		initialsInput.onValidateInput += delegate(string text, int charIndex, char addedChar) {
+			return ValidateInitials (addedChar);
+		};
+
+		initialsInput.Select ();
+	}
+
+	void ForceUpper () {
+		if (initialsInput.text != initialsInput.text.ToUpper ())
+			initialsInput.text = initialsInput.text.ToUpper ();
+	}
+
+	char ValidateInitials (char c) {
+		// If it's not a letter, return empty char.
+		return System.Char.IsLetter(c) ? c : '\0';
 	}
 }
