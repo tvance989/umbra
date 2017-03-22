ae756bbd916bca7093c7c7b8c013102033127acc
Taylor Vance <tvance989@gmail.com>
2017-03-22 07:19:08 -0500

    [WIP] Initial entry text field

---
+++ Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverManager : MonoBehaviour {
+	public Text scoreText, highScoreText;
+
+	void Start () {
+		scoreText.text = "Score: " + ScoreManager.instance.GetScore ();
+		highScoreText.text = "High Score: " + ScoreManager.instance.GetHighScore ();
+	}
+}
